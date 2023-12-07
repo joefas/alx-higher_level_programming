44bb115c18a2e6d6f2bea98a85d9eeff028f5fa8
joefas <ochienggeofrey06@gmail.com>
2023-12-07 02:30:43 -0800

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ counts = len(sys.argv) - 1
 if counts == 0:
     print("0 arguments.")
 elif counts == 1:
-    print("1 arguments:")
+    print("1 argument:")
 else:
     print("{} arguments:".format(counts))
-    for i in range(counts):
+for i in range(counts):
     print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     totaladd = 0
-    for i in range(len(sys.arg) - 1):
+    for i in range(len(sys.argv) - 1):
         totaladd += int(sys.argv[i + 1])
         print("{}".format(totaladd))
