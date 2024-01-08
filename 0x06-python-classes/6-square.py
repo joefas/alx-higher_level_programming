a04bacee67cf04806e09c80184c97b3ac4f98117
joefas <ochienggeofrey06@gmail.com>
2024-01-08 02:58:53 -0800

    coordinates of square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,7 +35,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or 
+        if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
@@ -49,7 +49,7 @@ class Square:
             return
 
         [print("") for i in range(0, self.__position[1])]
-        for i in range (0, self.__size):
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="")  for k in range(0, self.__size)]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
