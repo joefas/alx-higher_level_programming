d1263ea281dc13805872ae798a4b4f53eff94bbd
joefas <ochienggeofrey06@gmail.com>
2024-01-10 03:36:21 -0800

    string representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the perimeter of the Rectangle."""
-        if self.___width == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
